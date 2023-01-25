0a1bfbc4640bdc7cc0c5f627740bdf71d1fce404
Mike Whiting <mail@mikejw.co.uk>
2023-01-25 23:10:37 +0000

    add models

---
+++ src/Empathy/ELib/Storage/Role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Empathy\ELib\Storage;
+use Empathy\MVC\Entity as Entity;
+use Empathy\ELib\Model;
+
+class Role extends Entity
+{
+	const TABLE = 'role';
+
+    public $id;
+    public $name;
+
+    public function getIdByName($name)
+    {
+        $id = 0;
+        $sql = 'select id from '.Model::getTable('Role').' t1'
+            ." where name = '$name'";
+
+        $error = "Could not get role by name.";
+        $result = $this->query($sql, $error)->fetch();
+        if (isset($result['id']) && is_numeric($result['id'])) {
+            $id = $result['id'];
+        }
+        return $id;
+    }
+}
---
+++ src/Empathy/ELib/Storage/UserRole.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Empathy\ELib\Storage;
+use Empathy\MVC\Entity as Entity;
+use Empathy\ELib\Model;
+
+class UserRole extends Entity
+{
+	const TABLE = 'role_user';
+
+    public $id;
+    public $role_id;
+    public $user_id;
+
+
+	public function getRoles($id)
+	{
+        $sql = 'select t1.name from '.Model::getTable('Role').' t1,'
+            .' '.Model::getTable('UserRole').' t2'
+            ." where t2.user_id = '$id'"
+            .' and t1.id = t2.role_id';
+
+        $error = "Could not get user roles.";
+        $result = $this->query($sql, $error);
+        $roles = array();
+        foreach ($result as $index => $row) {
+        	$roles[$index] = $row['name'];
+        }
+        return $roles;
+	}
+}
